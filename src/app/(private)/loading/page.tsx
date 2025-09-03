76499b6c0b569a663f65c6b5001b0212c885e9de
Ewerton <ewerton.lfb@gmail.com>
2025-09-03 00:12:39 -0300

    retirada do redirecionamento do middlaware

--- src/app/(private)/loading/page.tsx
+++ src/app/(private)/loading/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import LoadInitialData from '@/components/common/LoadInitialData';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function Loading() {
