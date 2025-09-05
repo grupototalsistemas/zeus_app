24ef878eed740430c8f4dae4b157d1c97ff34ad6
Ewerton <ewerton.lfb@gmail.com>
2025-09-05 08:35:15 -0300

    problema de lint

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  console.log('=== MIDDLEWARE DEBUG ===');
-  console.log('URL:', request.nextUrl.pathname);
-  console.log('Cookies raw:', request.headers.get('cookie'));
+  // console.log('=== MIDDLEWARE DEBUG ===');
+  // console.log('URL:', request.nextUrl.pathname);
+  // console.log('Cookies raw:', request.headers.get('cookie'));
 
-  const token = request.cookies.get('zeus_token');
-  console.log('Token object:', token);
-  console.log('Token value:', token?.value);
+  const token = request.cookies.get('token');
+  // console.log('Token object:', token);
+  // console.log('Token value:', token?.value);
 
   // Lista TODOS os cookies
   const allCookies = request.cookies.getAll();
-  console.log('All cookies:', allCookies);
+  // console.log('All cookies:', allCookies);
 
   // if (!token?.value) {
   //   console.log('REDIRECIONANDO - Token não encontrado');
   //   return NextResponse.redirect(new URL('/signin', request.url));
   // }
 
-  console.log('PERMITINDO - Token encontrado');
+  // console.log('PERMITINDO - Token encontrado');
   return NextResponse.next();
 }
 
